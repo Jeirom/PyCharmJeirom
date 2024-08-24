00aa054163099ac71ea6557179ac05be14dd0d91
Даниил <jeiromkrizhanovskiyd@gmail.com>
2024-08-24 16:44:05 +0500

    Fixed. Add func in def mask_account_card. Change file masks.py

--- src/masks.py
+++ src/masks.py
@@ -3,17 +3,11 @@ from typing import Union
 
 def get_mask_card_number(number_card: Union[str]) -> str:
     # Функция маскирует средние числа номера карты, выводит результат
-    if len(number_card) == 16:
-        return f"{number_card[:4]} {number_card[4:6]} {'*' * 2} {'*' * 4} {number_card[12:]}"
-    else:
-        return "Ошибка. Неверное количество цифр."
+    return f"{number_card[:4]} {number_card[4:6]} {'*' * 2} {'*' * 4} {number_card[12:]}"
 
 
 def get_mask_account(account_number: Union[str]) -> str:
     # Функция маскрирует цифры номера счета, оставляя последние 4. Выводит резулат.
-    if len(account_number) == 20:
-        return f"{'*' * 2}{account_number[-4:]}"
-    else:
-        return "Ошибка. Неверное количество цифр."
+    return f"{'*' * 2}{account_number[-4:]}"
 
-#Тест для гита
+    # Тест для гита
--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,6 @@
 from typing import Union
+from masks import get_mask_account
+from masks import get_mask_card_number
 
 
 def mask_account_card(user_number: Union[str]) -> str:
@@ -11,20 +13,16 @@ def mask_account_card(user_number: Union[str]) -> str:
                 word.append(i)
             else:
                 number.append(i)
-        return f"{''.join(word).title()} {'*' * 2}{''.join(number)[-4:]}"
+        finaly_result_number = "".join(number).title()
+        return f"{''.join(word).title()} {get_mask_account(finaly_result_number)}"
     else:
         for i in user_number.replace(" ", ""):
             if i.isalpha():
                 word.append(i)
             else:
                 number.append(i)
-        return (
-            f"{''.join(word) + ' '} "
-            f"{''.join(number)[:4]} "
-            f"{''.join(number)[4:6]}{'*' * 2} "
-            f"{'*' * 4} "
-            f"{''.join(number)[12:]}"
-        )
+        finaly_result_card = "".join(number)
+        return f"{''.join(word) + ' '} " f"{get_mask_card_number(finaly_result_card)}"
 
 
 def get_date(time: Union[str]) -> str:
