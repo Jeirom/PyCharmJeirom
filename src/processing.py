fd7cc9824c363176856951649777ab25533e6204
Даниил <jeiromkrizhanovskiyd@gmail.com>
2024-08-26 23:46:15 +0500

    Исправленный вариант. Переписан README.md. Использованы литеры flake8, mypy

--- src/processing.py
+++ src/processing.py
@@ -1,14 +1,14 @@
-from typing import Union,Iterable
+from typing import Union
 
-def filter_by_state(data: list, state="EXECUTED") -> list:
+
+def filter_by_state(data: Union[list], state="EXECUTED") -> list:
     """Функция возвращает новый список словарей,
     содержащий только те словари, у которых ключ
         state соответствует указанному значению."""
     return [d for d in data if d.get("state") == state]
 
 
-def sort_by_date(list_of_dicts: list, reverse=True) -> list:
+def sort_by_date(list_of_dicts: Union[list], reverse=True) -> list:
     """Функция должна возвращать новый список,
-     отсортированный по дате (date)."""
+    отсортированный по дате (date)."""
     return sorted(list_of_dicts, key=lambda x: x["date"], reverse=reverse)
-
